ac55fb571a97a69a9dbfecd67a818c32d0f3506e
Pongkot Sae-li <pongkot.saeli@outlook.com>
2020-08-09 13:05:35 +0700

    init test and lint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/no-unresolved': 0,
+    'import/extensions': 'off'
+  },
+};
